a1b28696225ae3f8ddd2675a1732053121b43e8e
Trinidad <trinimamud@gmail.com>
2023-07-23 21:57:00 -0300

    registrarse

--- src/app/core/layout/registration/registration.component.ts
+++ src/app/core/layout/registration/registration.component.ts
@@ -1,6 +1,8 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { AuthenticationService } from '../../services/authentication.service';
+import { UtilService } from '../../services/util.service';
 import { SignInModalComponent } from '../sign-in-modal/sign-in-modal.component';
 
 @Component({
@@ -21,7 +23,11 @@ export class RegistrationComponent implements OnInit, AfterViewInit {
   currentUser: any = {};
   showPass = false;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(
+    private dialog: MatDialog,
+    private authenticationService: AuthenticationService,
+    private utilService: UtilService
+  ) {}
     
 
   ngOnInit(): void {
@@ -49,7 +55,25 @@ export class RegistrationComponent implements OnInit, AfterViewInit {
     })
   }
 
-  register() {
+  async register() {
+    const data = {
+      name: this.registrationForm.controls.name.value!,
+      surename: this.registrationForm.controls.surename.value!,
+      email: this.registrationForm.controls.email.value!,
+      password: this.registrationForm.controls.password.value!,
+      birthday: this.registrationForm.controls.birthday.value!
+    }
 
+    this.authenticationService.register(data)
+    .subscribe((currentUser: any) => {
+        
+        if (!currentUser) {
+          this.utilService.notification('No se pudo obtener el usuario', 'error');
+          return;
+        }
+      },
+      (error: any) => {
+        this.utilService.notification('Email o contraseña incorrectos', 'error')
+      });
   }
 }
