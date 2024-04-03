bd5b1c2b033dfdfde29b4cebd60df80960e9d9da
eliaslamanna <eliaslamanna@hotmail.com>
2024-04-03 02:17:42 -0300

    fix deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  baseUrl: 'https://wiggly-backend-production.up.railway.app',
+  production: false,
+  baseUrl: 'http://localhost:8080',
 };
 /*
  * For easier debugging in development mode, you can import the following file
